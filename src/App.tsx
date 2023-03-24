340293a2d78b477ff4e3b2978c703e96d210ce0a
Iain Grant <iain.grant@sepa.org.uk>
2023-03-24 14:15:16 +0000

    fixing route

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       <NavBar />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={
+          <Route path='/*' element={
             <div>
               <Search />
               <CountriesList />
